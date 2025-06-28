8b0eae2f0f923ec061c0067caed5e3505ffe14cd
Jonathan Matus <jonathan.matus.vidal@gmail.com>
2025-06-27 22:52:38 -0400

    Eliminar archivos antiguos y organizar estructura

--- script.js
+++
@@ -1,7 +0,0 @@
-// Pequeña interacción: Al hacer clic en la foto, mostrar alerta con saludo
-
-const profilePic = document.querySelector('.profile-pic');
-
-profilePic.addEventListener('click', () => {
-  alert('¡Hola! Gracias por visitar mi portafolio.');
-});
